df989fa516db5228170e9065f88108f2a66d5472
qwantanius <borshrawl@gmail.com>
2021-12-07 14:11:04 +0300

    v0

--- db.py
+++ db.py
@@ -1,4 +1,31 @@
-import sqlite3,json
+import sqlite3,json,copy
+
+user_tmp = dict({
+  "tg_id": "NULL",
+  "name": "NULL",
+  "login": "NULL",
+  "pwd": "NULL",
+  "phone": "NULL",
+  "status": "NULL",
+  "passport_id": "NULL",
+  "pesel_id": "NULL",
+  "contract_starts_at": "NULL",
+  "contract_ends_at": "NULL",
+  "visa_created_at": "NULL",
+  "visa_expires_at": "NULL",
+  "vocation_start_at": "NULL",
+  "vocation_ends_at": "NULL",
+  "is_admin": "NULL",
+  "is_on_vocation": "NULL",
+  "is_vocation_allowed_by_admin": "NULL",
+  "vocation_allowed_by_admin_starts_at": "NULL",
+  "vocation_allowed_by_admin_ends_at": "NULL",
+  "cadence_starts_at": "NULL",
+  "cadence_ends_at": "NULL",
+  "statements_data": []
+})
+
+
 
 #БАЗОВЫЕ ПРОЦЕДУРЫ И КОНФИГУРАЦИЯ
 INSERT_TMP = """insert into users values('{}', '{}');"""
@@ -14,12 +41,24 @@ SER =         lambda user:json.dumps(user)
 DES =         lambda user:json.loads(user)
 MIGRATE =     lambda:SQL(TABLE_SCH)
 COMMIT =      lambda:DATABASE.commit()
-SAVE_USER =   lambda tgid,u:SQL(INSERT_TMP.format(u,tgid))
+SAVE_USER =   lambda su,tgid:SQL(INSERT_TMP.format(su,tgid))
 GET_USER =    lambda tgid: SQL(SELECT_TMP.format(tgid))
 GET_UTGID =   lambda fobj: fobj[0][1]
 GET_UOBJ =    lambda fobj: DES(fobj[0][0])
-UPDATE_USER = lambda tgid,u:SQL(UPDATE_TMP.format(u,tgid))
+UPDATE_USER = lambda u,tgid:SQL(UPDATE_TMP.format(u,tgid))
 DELETE_USER = lambda tgid:SQL(DELETE_TMP.format(tgid))
 DUMP =        lambda: SQL("select * from users")
+
+
+def USER(u):
+    _user = copy.deepcopy(user_tmp)
+    _user.update(u)
+    return _user
+
+try:
+    MIGRATE()
+    COMMIT()
+except:
+    print("No migration needed")
 
 
